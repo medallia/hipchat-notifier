3bb70c767d3b34ee3a8f55d62159ff0a342aa6e5
Chris Verges <cverges@medallia.com>
2016-04-17 05:56:16 -0700

    Pre-release cleanups

--- bin/hipchat-notifier.js
+++ bin/hipchat-notifier.js
@@ -1,36 +1,41 @@
 #!/usr/bin/env node
 
+/**
+ * @copyright Copyright © 2016 Medalli,a Inc. All rights reserved.
+ * @license Licensed under the MIT License (MIT).
+ */
+
 var argv = require('argv');
 
 const configFile = {
-    name: "config",
-    short: "c",
-    type: "path",
-    description: "Specify the configuration json file",
-    example: "--config=./config.json or -c ./config.json"
+    name: 'config',
+    short: 'c',
+    type: 'path',
+    description: 'Specify the configuration json file',
+    example: '--config=./config.json or -c ./config.json'
 };
 
 const template = {
-    name: "template",
-    short: "t",
-    type: "path",
-    description: "Specify the message template file",
-    example: "--template=./message.txt or -t ./message.txt"
+    name: 'template',
+    short: 't',
+    type: 'path',
+    description: 'Specify the message template file',
+    example: '--template=./message.txt or -t ./message.txt'
 };
 
 
 const message = {
-    name: "message",
-    short: "m",
-    type: "string",
-    description: "Specify the message",
-    example: "--message=\"Hi @all !\" or -m \"Hi @all !\""
+    name: 'message',
+    short: 'm',
+    type: 'string',
+    description: 'Specify the message',
+    example: '--message=\'Hi @all !\' or -m \'Hi @all !\''
 };
 
 argv.option(configFile);
 argv.option(template);
 argv.option(message);
-argv.version("1.1.0");
+argv.version('1.1.1');
 
 const args = argv.run();
 

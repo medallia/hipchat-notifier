70b14fdb43ae0262ff21425235fbfa4cd4ab0f30
Ezequiel Bergamaschi <ebergamaschi@medallia.com>
2016-04-17 02:06:32 -0300

    adding copyright to source code

--- example/app.js
+++ example/app.js
@@ -1,7 +1,11 @@
+/**
+ * @copyright Copyright © 2016 Medallia Inc. All rights reserved.
+ * @license Licensed under the MIT License (MIT).
+ */
 var notify = require('medallia-hipchat-notifier');
 
 var context = {
   "DAY_MOMENT": "morning"
-}
+};
 
 notify("./config.json", "./template.msg", context);
